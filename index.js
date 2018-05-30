547027e2d362e5384ca1a400ab71cef9db4a757f
khubbard96 <kevlar2020@gmail.com>
2018-05-30 18:24:51 -0500

    f

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ var router = new director.http.Router({
 
 function check_bank(id){
   console.log(id);
-  console.log(this.req.chunks[0]);
+  console.log(this.req.chunks);
   //var message = this.req.chunks[0].text;
   //var message = this.req.chunks["text"];//groupme v3 message object
   //var sender = this.req.chunks[0]["user_id"];
